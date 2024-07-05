cc9b35def49c0645833c81978fb69fccf0bac22d
serping <seadfeng@icloud.com>
2024-07-05 11:46:17 +0800

    update

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,6 @@ import axios, { AxiosInstance } from 'axios';
 import config from "./config";
 import {SerpingConfig} from "./types";
 import { GoogleSerpSearchParam } from "./zod/google/base";
-import { TIMEOUT } from 'dns';
 
 class Serping {
   private axiosInstance: AxiosInstance;
